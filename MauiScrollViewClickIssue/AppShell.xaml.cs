76c5f987108e81f892b1fa42cdfa22306c5e4fb2
ili <chudinia@gmail.com>
2022-07-15 14:01:11 +0500

    iOS ScrollView bug sample

---
+++ MauiScrollViewClickIssue/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiScrollViewClickIssue;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiScrollViewClickIssue/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiScrollViewClickIssue;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+		var btn = (Button)sender;
+		var separator = btn.Text.IndexOf("|");
+
+		btn.Text = btn.Text.Substring(0, separator+1) + $" {count}";
+	}
+}
+
